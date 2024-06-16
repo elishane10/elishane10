1d2d8e78c9a5f482a439a94b19751efc17fa438a
elishane10 <beheartless786@gmail.com>
2024-06-17 02:41:35 +0530

    Dsa basic
    Basic dsa ques

---
+++ perimeter.py
@@ -0,0 +1,6 @@
+le =int(input("Enter the length"))
+wi = int(input("Enter the width"))
+if le == wi :
+    print("Its a square, so the area of the square will be =",4*le)
+else:
+    print("It is a rectangle not square, the perimeter of the rectangle is =",2*(le+wi))
---
+++ positve,-ve or 0.py
@@ -0,0 +1,7 @@
+num=int(input("Enter the no ="))
+if (num > 0):
+    print("The no is a positive no")
+elif (num ==0):
+    print("The no is equal to 0")
+else:
+    print("The no is a -ve no")
